58b2ef87427ab3fe95f470c45c868cd74ec229a4
Jason Huntington <jasonmhuntington@gmail.com>
2013-03-05 08:09:13 -0600

    fix weather bug and add notes

---
+++ @Resources/Measures/Notes.lua
@@ -0,0 +1,62 @@
+function Initialize()
+	sVariablePrefix = SELF:GetOption('VariablePrefix','')
+	sContentDivider = SELF:GetOption('ContentDivider','')
+	sFinishAction = SELF:GetOption('FinishAction')
+
+	GetFiles()
+
+	iCurrentNote=1
+end
+
+function Update()
+	local sNotePath=assert(tNotes[iCurrentNote],'Invalid Note number '..iCurrentNote)
+	SKIN:Bang('!SetVariable','CurrentNote',iCurrentNote)
+	
+	-----------------------------------------------------------------------
+	-- DETERMINE CONTENT
+	
+	local sNoteDir,sNoteName,sNoteExt = string.match(sNotePath, '(.-)([^\\]-)%.([^%.]+)$')
+	
+	local sRawFile=io.input(sNotePath)
+	local sRaw=io.read('*all')
+	io.close(sRawFile)
+	
+	local sNoteContent=string.gsub(sRaw,sContentDivider..'.*','')
+	
+	--sNoteContent = (sContentDivider~='' and string.match(sRaw,sContentDivider)) and string.match(sRaw,'(.-)'..sContentDivider) or sRaw
+		
+	for k,v in pairs({ -- OUTPUT
+		Path=sNotePath,
+		Name=sNoteName,
+		Content=string.gsub(sNoteContent,'- ','· '),
+	}) do SKIN:Bang('!SetVariable',sVariablePrefix..k,v) end	
+	
+	if sFinishAction ~= '' then -- FINISH ACTION
+		SKIN:Bang(sFinishAction)
+	end
+	
+	return 'Success!'	
+end
+
+function GetFiles()
+	tNotes = {}
+	for a in string.gmatch(SELF:GetOption('NotePath',''),'[^%|]+') do
+		table.insert(tNotes,SKIN:MakePathAbsolute(a))
+	end
+end
+
+function SwitchToPrevious()
+	iCurrentNote = iCurrentNote==1 and #tNotes or iCurrentNote-1
+	Update()
+end
+
+function SwitchToNext()
+	iCurrentNote = iCurrentNote % #tNotes + 1
+	Update()
+end
+
+function NoteError(sErrorPath, sErrorName, sErrorContent)
+	SKIN:Bang('!SetVariable',sVariablePrefix..'Path',sErrorPath)
+	SKIN:Bang('!SetVariable',sVariablePrefix..'Name',sErrorName)
+	SKIN:Bang('!SetVariable',sVariablePrefix..'Content',sErrorContent)
+end
---
+++ @Resources/Measures/Tabs.lua
@@ -0,0 +1,46 @@
+function Initialize()
+	NumberOfTabs = tonumber(SKIN:GetVariable('NumberOfTabs'))
+	Type = SELF:GetOption('Type', 'Reader')
+	TotalTabs = SELF:GetNumberOption('TotalTabs', 8)
+end
+
+function Update()
+	if NumberOfTabs > 1 then
+		for i = 2, NumberOfTabs do
+			SKIN:Bang('!EnableMeasureGroup', 'Tab'..i)
+			SKIN:Bang('!ShowMeterGroup', 'Tab'..i)
+		end
+
+		if Type == 'Reader' then
+			UpdateReader()
+		elseif Type == 'Notes' then
+			UpdateNotes()
+		end
+
+		SKIN:Bang('!Update')
+	end
+
+	for i = NumberOfTabs + 1, TotalTabs do
+		for j,v in ipairs{ 'X', 'Y', 'W', 'H' } do
+			SKIN:Bang('!SetOptionGroup', 'Tab'..i, v, 0)
+		end
+	end
+end
+
+function UpdateReader()
+	local Measures = {'MeasureFeed1'}
+	for i = 2, NumberOfTabs do
+		table.insert(Measures, 'MeasureFeed'..i)
+	end
+	SKIN:Bang('!SetOption', 'MeasureScriptReader', 'FeedMeasureName', table.concat(Measures, '|'))
+	SKIN:Bang('!CommandMeasure', 'MeasureScriptReader', 'GetMeasures()')
+end
+
+function UpdateNotes()
+	local Files = { SKIN:GetVariable('NotesFile1') }
+	for i = 2, NumberOfTabs do
+		table.insert(Files, SKIN:GetVariable('NotesFile'..i))
+	end
+	SKIN:Bang('!SetOption', 'MeasureNote', 'NotePath', table.concat(Files, '|'))
+	SKIN:Bang('!CommandMeasure', 'MeasureNote', 'GetFiles()')
+end
